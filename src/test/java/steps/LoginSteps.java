f3085e7981a52cdf8bc0a712d14138766e63c42a
kloginov-biarum <kloginov@biarum.com>
2023-09-05 21:59:29 +0200

    fix

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,13 +1,19 @@
 package steps;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.LoginPage;
 
 import java.time.Duration;
 
 public class LoginSteps {
     ChromeDriver driver;
+
     @Given("^I open the page Login$")
     public void iOpenThePageLogin() {
         ChromeOptions options = new ChromeOptions();
@@ -17,6 +23,12 @@ public class LoginSteps {
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.saucedemo.com/");
+    }
+
+    @When("^I fill input user name \"([^\"]*)\"$")
+    public void iFillInputUserName(String usernameValue){
+      LoginPage loginPage = new LoginPage(driver);
+      loginPage.usernameInputField.sendKeys(usernameValue);
     }
 }
 
