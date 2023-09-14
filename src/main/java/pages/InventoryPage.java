1e71b48157300d8a61687d2bd6118bc912aa628a
Michasik77 <michael.gadatzki@gmail.com>
2023-09-14 20:44:17 +0200

    Added new Test

--- src/main/java/pages/InventoryPage.java
+++ src/main/java/pages/InventoryPage.java
@@ -1,20 +1,13 @@
 package pages;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selectors.byClassName;
+import static com.codeborne.selenide.Selenide.$;
 
 public class InventoryPage {
 
-    public InventoryPage(WebDriver driver) {
-        PageFactory.initElements(driver, this);
-    }
-
-    @FindBy(className = "inventory_list")
-    public WebElement inventoryList;
-
-
-
+    public SelenideElement inventoryList = $(byClassName("inventory_list"));
 
 }
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,80 +1,40 @@
 package steps;
 
-import cucumber.api.java.After;
+import com.codeborne.selenide.Condition;
 import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.InventoryPage;
-import pages.LoginPage;
+import static com.codeborne.selenide.Condition.text;
 
-import java.time.Duration;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-public class LoginSteps {
-    ChromeDriver driver;
-
-
-    @Given("^I open the page Login$")
-    public void iOpenThePageLogin() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get("https://www.saucedemo.com/");
-    }
+public class LoginSteps extends BaseSteps{
 
     @When("^I fill input user name \"([^\"]*)\"$")
     public void iFillInputUserName(String usernameValue) {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.usernameInputField.sendKeys(usernameValue);
+        loginPage.usernameInputField.setValue(usernameValue);
     }
 
     @Then("^I fill input password \"([^\"]*)\"$")
     public void iFillInputPassword(String passwordValue) {
-        LoginPage loginPage = new LoginPage(driver);
-        loginPage.passwordInputField.sendKeys(passwordValue);
+        loginPage.passwordInputField.setValue(passwordValue);
     }
 
     @And("^I click on button Login$")
     public void iClickOnButtonLogin() {
-        LoginPage loginPage = new LoginPage(driver);
         loginPage.loginButton.click();
     }
 
     @Then("^The page Inventory is displayed$")
     public void thePageInventoryIsDisplayed() {
-        InventoryPage inventoryPage = new InventoryPage(driver);
-        assertTrue(inventoryPage.inventoryList.isDisplayed());
+        inventoryPage.inventoryList.shouldBe(Condition.visible);
     }
 
-    @Then("^error message is displayed$")
-    public void errorMessageIsDisplayed() {
-        LoginPage loginPage = new LoginPage(driver);
-        assertTrue(loginPage.errorMessage.isDisplayed());
-    }
-
-    @And("^error message about invalid creds has correct text$")
-    public void errorMessageAboutInvalidCredsHasCorrectText() {
-        LoginPage loginPage = new LoginPage(driver);
-        assertEquals("Epic sadface: Username and password do not match any user in this service",
-                loginPage.errorMessage.getText());
-    }
-
-    @And("^error message with text \"([^\"]*)\" is displayed$")
+    @Then("^error message with text \"([^\"]*)\" is displayed$")
     public void errorMessageWithTextIsDisplayed(String expectedText) {
-        LoginPage loginPage = new LoginPage(driver);
-        assertEquals(expectedText, loginPage.errorMessage.getText());
+        loginPage.errorMessage.shouldHave(text(expectedText));
     }
 
-    @After
-    public void tearDown() {
-        driver.quit();
-    }
+
+
 }
 
 
