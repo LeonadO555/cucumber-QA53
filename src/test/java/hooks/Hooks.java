c2c7bc1c42056b2dd706ca551420342d7b274594
Michasik77 <michael.gadatzki@gmail.com>
2023-09-14 20:00:49 +0200

    Added new Scenario

---
+++ src/test/java/hooks/Hooks.java
@@ -0,0 +1,24 @@
+package hooks;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class Hooks {
+
+    @Before
+    public void setUp(){
+        System.out.println("---Before methode started---");
+        System.setProperty("chromeoptions.args", "--remote-allow-origins=*");
+        open("https://www.saucedemo.com/");
+        clearBrowserCookies();
+        clearBrowserLocalStorage();
+        System.out.println("---Before methode finished---");
+    }
+
+    @After
+    public void tearDown(){
+        closeWebDriver();
+    }
+}
